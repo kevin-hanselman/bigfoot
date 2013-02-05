dc266addc4916a12d254577ec80461cde18a135d
Kevin Hanselman <kevin.hanselman@gmail.com>
2013-02-04 23:12:11 -0500

    working on footprint pad access, added batch file mode

--- bigfoot.py
+++ bigfoot.py
@@ -43,7 +43,7 @@ class Pad:
 						(self._x - xh, self._y + yh))
 		
 class Footprint:
-	def __init__(self, name='newfp', pads=None):
+	def __init__(self, name='new', pads=None):
 		self.name = name
 		if pads:
 			self._pads = pads
@@ -64,44 +64,129 @@ class Footprint:
 		
 	def add_pad(self, pad):
 		self._pads.append(pad)
+		
+	def del_pad(self, i):
+		if self.assert_pads(i):
+			del self._pads[i]
 
+	def get_pads(self, p):
+		'''Returns either a list of pads or a single Pad.'''
+		if self.assert_pads(p):
+			if type(p) == type(list()):
+				out = []
+				for i in p:
+					out.append(self._pads[i])
+				return out
+			else:
+				return self._pads[p]
+				
+	def assert_pads(self, p):
+		if type(p) == type(list()):
+			for i in p:
+				try:
+					self._pads[i]
+				except IndexError:
+					print('Footprint %s has no pad numbered %d' % (self.name, i))
+					return False
+		else:
+			try:
+				self._pads[p]
+			except IndexError:
+				print('Footprint %s has no pad numbered %d' % (self.name, p))
+				return False
+		return True
+			
 def main():
+	'''Entry point for program.'''
 	global fp
 	topparser = init_topparser()	# parser for program start
 	args = topparser.parse_args()
-	
+		
 	cmdparser = init_cmdparser()	# parser for program command-prompt
 	
 	fp = Footprint()
+	
+	if args.batch:
+		for line in args.batch:
+			#print(repr(line))
+			if line.strip():
+				try:
+					exec_cmd(cmdparser, line)
+				except SystemExit:
+					pass
+	
 	while(True):
-		s = input('BGFT: %s >> ' % fp.name)
 		try:
-			args = cmdparser.parse_args(s.split())
-		except SystemExit as ex:
+			s = input('BGFT: %s >> ' % fp.name)
+		except KeyboardInterrupt:
+			print()
+			sys.exit(0)
+		try:
+			exec_cmd(cmdparser, s)
+		except SystemExit:
 			continue
-		args.func(args)
+		
+def exec_cmd(parser, str):
+	try:
+		args = parser.parse_args(str.split())
+	except Exception as ex:
+		print(str(ex))
+	args.func(args)
 
 def cmd_quit(args):
+	'''Quit bigfoot.py.
+	input:
+		args = argparse Namespace
+	'''
 	sys.exit(0)
 
 def cmd_add(args):
+	'''Adds a pad to the current footprint.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
 	p = Pad(xsiz=args.w, ysiz=args.h, xpos=args.x, ypos=args.y)
 	fp.add_pad(p)
 
 def cmd_edit(args):
+	'''Edit the attributes of a pad on the current footprint.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
-	p = fp._pads[args.pad].change(xsiz=args.w, ysiz=args.h, xpos=args.x, ypos=args.y)
+	if args.pad == 0:
+		print('Cannot edit origin (Pad 0).')
+	else:
+		p = fp.get_pads(args.pad)
+		p.change(xsiz=args.w, ysiz=args.h, xpos=args.x, ypos=args.y)
 
 def cmd_del(args):
+	'''Deletes a pad from the current footprint.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
-	del fp._pads[args.pad]
+	if args.pad == 0:
+		print('Cannot delete origin (Pad 0).')
+	else:
+		fp.del_pad(args.pad)
 
 def cmd_ls(args):
+	'''List all pads on a given footprint.
+	input:
+		args = argparse Namespace
+	output:
+		print()
+	'''
 	global fp
 	fp.display()
 	
 def cmd_save(args):
+	'''Saves a footprint to a file.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
 	if args.out:
 		fp.name, fext = os.path.splitext(os.path.basename(args.out))
@@ -116,6 +201,10 @@ def cmd_save(args):
 		print(str(ex))
 	
 def cmd_load(args):
+	'''Loads a footprint from a file.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
 	fext = os.path.splitext(args.path)[1]
 	if not fext:
@@ -126,26 +215,29 @@ def cmd_load(args):
 		print(str(ex))
 
 def cmd_pos(args):
+	'''Places a pad relative to another pad.
+	input:
+		args = argparse Namespace
+	'''
 	global fp
-	
 	offset = [None, None]
 	if args.dir == 'N' or args.dir == 'S': # vertical orientation
 		for i,(pi,r) in enumerate(zip(args.pad, args.ref)):
 			#print('%d %d %s' % (i, pi, r))
-			p = fp._pads[pi]
+			p = fp.get_pads(pi)
 			if r == 'f':
-				offset[i] = p._h/2.0
-			elif r == 'n':
 				offset[i] = -p._h/2.0
+			elif r == 'n':
+				offset[i] = p._h/2.0
 			elif r == 'c':
 				offset[i] = 0
 	elif args.dir == 'E' or args.dir == 'W': # horizontal orientation
 		for i,(pi,r) in enumerate(zip(args.pad, args.ref)):
-			p = fp._pads[pi]
+			p = fp.get_pads(pi)
 			if r == 'f':
-				offset[i] = p._w/2.0
-			elif r == 'n':
 				offset[i] = -p._w/2.0
+			elif r == 'n':
+				offset[i] = p._w/2.0
 			elif r == 'c':
 				offset[i] = 0
 	d = sum(offset)
@@ -163,36 +255,45 @@ def cmd_pos(args):
 		fp._pads[args.pad[0]].change(xpos=d)
 
 def cmd_dist(args):
+	'''Compute the x and y distance between two reference points on two pads.
+	input:
+		args = argparse Namespace
+	output:
+		print()
+	'''
 	global fp
-	pd = get_pads(args.pad)
+	pd = fp.get_pads(args.pad)
 	points = []
 	for r,p in zip(args.ref, pd):
 		if r == 0: # center
 			points.append((p._x, p._y))
 		else: # corner
-			points.append(p._corn(r-1))
+			points.append(p._corn[r-1])
 	d = twopoint_dist(points)
 	print('dx = %.4f, dy = %.4f' % (d[0], d[1]))
-			
-def get_pads(listp):
-	global fp
-	out = []
-	for i in listp:
-		out.append(fp._pads[i])
-	return out
-	
+
 def twopoint_dist(pts):
+	'''Compute the x and y distance between two points.
+	input:
+		pts = a 2-element tuple containing 2-element tuples
+	output:
+		2-element tuple
+	'''
 	return (pts[0][0]-pts[1][0], pts[0][1]-pts[1][1])
 	
-def init_topparser():
+def init_topparser(): # top-level script arg parser
 	parser = argparse.ArgumentParser(description=__doc__)
 	parser.add_argument('-V','--version',
 						help='display the version of %(prog)s',
 						action='version',	
 						version='%(prog)s 0.1')
+	parser.add_argument('-b','--batch',
+						help='input batch file',
+						metavar='FILE',
+						type=argparse.FileType('r'))
 	return parser
 	
-def init_cmdparser():
+def init_cmdparser(): # script commands arg parser
 	parser = argparse.ArgumentParser()
 	subs = parser.add_subparsers()
 	
